b319a56be838ff8763dadeef66b8519fc6fbe549
GabyDevX <gabo.ramirez0811@gmail.com>
2023-03-03 11:11:54 -0300

    Added some changes to fix some issues

--- controller/mongo.js
+++ controller/mongo.js
@@ -1,187 +1,176 @@
-import mongoose from'mongoose'
-import * as modelProd from "./../models/producto.js"
-import * as modelCar from "./../models/carrito.js"
+import mongoose from "mongoose";
+import * as modelProd from "./../models/producto.js";
+import * as modelCar from "./../models/carrito.js";
 
 class Mongo {
+  constructor(url, type) {
+    (this.url = url), (this.type = type);
+  }
 
-    constructor(url, type) {
-        this.url = url,
-        this.type = type
+  async connect() {
+    try {
+      await mongoose.connect(this.url, {
+        useNewUrlParser: true,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async connect() {
-        try {
-            
-            await mongoose.connect(this.url,
-            {
-            useNewUrlParser: true,
-            })
+  async getById(id, tipo = this.type) {
+    try {
+      this.connect();
 
-        } catch (error) {
-            console.log(error);
+      if (tipo == "producto") {
+        const product = await modelProd.productos.find({ _id: id });
+
+        return product;
+      } else {
+        const cart = await modelCar.carritos.find({ _id: id });
+        console.log(cart);
+        return cart;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      this.connect();
+
+      if (this.type == "producto") {
+        const product = await modelProd.productos.deleteOne({ _id: id });
+
+        console.log(product);
+
+        return product;
+      } else {
+        const cart = await modelCar.carritos.deleteOne({ _id: id });
+
+        console.log(cart);
+
+        return product;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateById(id, newData) {
+    try {
+      this.connect();
+
+      if (this.type == "producto") {
+        const product = await modelProd.productos.updateOne(
+          { _id: id },
+          {
+            $set: { ...newData },
+          }
+        );
+
+        console.log(product);
+        return product;
+      } else {
+        const cart = await modelCar.carritos.updateOne(
+          { _id: id },
+          {
+            $set: { ...newData },
+          }
+        );
+
+        console.log(cart);
+        return cart;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async addToArrayById(id, objectToAddId) {
+    try {
+      this.connect();
+
+      const carrito = await modelCar.carritos.findOne({ _id: id });
+
+      const product = await modelProd.productos.findOne({ _id: objectToAddId });
+
+      carrito.products.push(JSON.parse(JSON.stringify(product)));
+
+      const updated = await modelCar.carritos.updateOne(
+        { _id: id },
+        {
+          $set: { ...carrito },
         }
-        
+      );
+
+      return updated;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async getById(id, tipo = this.type) {
+  async removeFromArrayById(id, objectToRemoveId, keyName) {
+    const carrito = await modelCar.carritos.findOne({ _id: id });
 
-        try {
-            
-            this.connect()
-    
-            if(tipo == "producto"){
-    
-                const product = await modelProd.productos.find({_id : id})
+    console.log(carrito.products);
 
-                return product
-            
-            } else {
-    
-                const cart = await modelCar.carritos.find({_id : id})
-                console.log(cart);
-                return cart
-    
-            }
+    carrito.products = carrito.products.filter(
+      (e) => e._id !== objectToRemoveId
+    );
 
-        } catch (error) {
-            console.log(error);
-        }
-        
+    console.log(carrito.products);
+
+    const updated = await modelCar.carritos.updateOne(
+      { _id: id },
+      {
+        $set: { ...carrito },
+      }
+    );
+
+    return updated;
+  }
+
+  async save(object) {
+    try {
+      this.connect();
+
+      if (this.type == "producto") {
+        const saveProduct = new modelProd.productos(object);
+
+        const saved = await saveProduct.save();
+
+        console.log(saved._id);
+        return saved._id;
+      } else {
+        const saveCart = new modelCar.carritos(object);
+
+        const saved = await saveCart.save();
+
+        console.log(saved._id);
+        return saved._id;
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async deleteById(id) {
+  async getAll() {
+    try {
+      this.connect();
 
-        try {
-            
-            this.connect()
-    
-            if(this.type == "producto"){
-    
-                const product = await modelProd.productos.deleteOne({_id : id})
-    
-                console.log(product);
-
-                return product
-            
-            } else {
-    
-                const cart = await modelCar.carritos.deleteOne({_id : id})
-        
-                console.log(cart);
-
-                return product
-    
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-        
-        
+      if (this.type == "producto") {
+        const product = await modelProd.productos.find({}, { __v: 0 }).lean();
+        console.log(product);
+        return product;
+      } else if (this.type == "carrito") {
+        const cart = await modelCar.carritos.find({}, { __v: 0 }).lean();
+        return cart;
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    async updateById(id, newData) {
-
-        try {
-            
-            this.connect()
-    
-            if(this.type == "producto"){
-    
-               const product = await modelProd.productos.updateOne({_id : id}, {
-               $set: {...newData}
-               })
-    
-               console.log(product);
-               return product
-           
-           } else {
-    
-               const cart = await modelCar.carritos.updateOne({_id : id}, {
-               $set: {...newData}
-               })
-       
-               console.log(cart);
-               return cart
-
-           }
-
-        } catch (error) {
-            console.log(error);
-        }
-        
-    }
-
-    async addToArrayById(id, objectToAddId) {
-        try {
-            this.connect()
-
-            const carrito = await modelCar.carritos.findOne({_id : id})
-
-            const product = await modelProd.productos.findOne({_id : objectToAddId})
-
-            carrito.products.push(JSON.parse(JSON.stringify(product)))
-
-            const updated = await modelCar.carritos.updateOne({_id : id}, {
-               $set: {...carrito}
-               })      
-               
-            return updated
-            
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async removeFromArrayById(id, objectToRemoveId, keyName) {
-        const carrito = await modelCar.carritos.findOne({_id : id})
-
-        console.log(carrito.products);
-
-        carrito.products = carrito.products.filter(e => e._id !== objectToRemoveId)
-
-        console.log(carrito.products);
-
-        const updated = await modelCar.carritos.updateOne({_id : id}, {
-               $set: {...carrito}
-               })
-
-        return updated
-    }
-
-    async save(object) {
-
-        try {
-            
-            this.connect()
-    
-            if(this.type == "producto"){
-    
-                const saveProduct = new modelProd.productos(object)
-    
-                const saved = await saveProduct.save()
-    
-                console.log(saved._id);
-                return saved._id
-            
-            } else {
-    
-                const saveCart = new modelCar.carritos(object)
-                
-                const saved = await saveCart.save()
-                
-                console.log(saved._id);
-                return saved._id
-    
-            }
-
-
-        } catch (error) {
-            console.log(error);
-        }
-
-    }
-
+  }
 }
 
-export const MongoDB = Mongo
+export const MongoDB = Mongo;
--- routes/productos.js
+++ routes/productos.js
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router } from "express";
 
 // import {Container} from '../controller/contenedor.js'
 // const contenedor = new Container('./files/productos', [
@@ -11,102 +11,104 @@ import { Router } from 'express'
 //   'timestamp',
 // ])
 
-import {FirebaseDB} from '../controller/firebase.js'
-const fb = new FirebaseDB('producto')
+import { FirebaseDB } from "../controller/firebase.js";
+const fb = new FirebaseDB("producto");
 
-import {MongoDB} from '../controller/mongo.js'
-const db = new MongoDB('mongodb+srv://coderhouse:coderhouse@cluster0.6zhqh8c.mongodb.net/?retryWrites=true&w=majority', 'producto') 
+import { MongoDB } from "../controller/mongo.js";
+const db = new MongoDB(
+  "mongodb+srv://coderhouse:coderhouse@cluster0.6zhqh8c.mongodb.net/?retryWrites=true&w=majority",
+  "producto"
+);
 
-const router = Router()
+const router = Router();
 
-const admin = true
+const admin = true;
 
 /* ------------------------ Product Endpoints ------------------------ */
 
 // GET api/productos
-router.get('/:id?', async (req, res) => {
-  const { id } = req.params
+router.get("/:id?", async (req, res) => {
+  const { id } = req.params;
   if (id) {
     // const product = await contenedor.getById(id)
     // const product = await fb.getById(id)
-    const product = await db.getById(id)
+    const product = await db.getById(id);
 
     product
       ? res.status(200).json(product)
-      : res.status(400).json({ error: 'product not found' })
+      : res.status(400).json({ error: "product not found" });
   } else {
-    // const products = await contenedor.getAll()
+    const products = await db.getAll();
     //work
-    res.status(200)
+    res.status(200);
     // res.status(200).json(products)
   }
-})
+});
 
 // POST api/productos
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   if (admin) {
-    const { body } = req
+    const { body } = req;
 
-    body.timestamp = new Date().toLocaleString()
+    body.timestamp = new Date().toLocaleString();
 
     // const newProductId = await contenedor.save(body)
     // const newProductId = await fb.save(body)
-    const newProductId = await db.save(body)
+    const newProductId = await db.save(body);
 
     newProductId
       ? res
           .status(200)
-          .json({ success: 'product added with ID: ' + newProductId })
+          .json({ success: "product added with ID: " + newProductId })
       : res
           .status(400)
-          .json({ error: 'invalid key. Please verify the body content' })
+          .json({ error: "invalid key. Please verify the body content" });
   } else {
     res.json({
       error: -1,
-      descripcion: 'ruta api/productos metodo post no autorizada',
-    })
+      descripcion: "ruta api/productos metodo post no autorizada",
+    });
   }
-})
+});
 
 // PUT api/productos/:id
-router.put('/:id', async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   if (admin) {
-    const { id } = req.params
-    const { body } = req
+    const { id } = req.params;
+    const { body } = req;
 
     // const wasUpdated = await contenedor.updateById(id, body)
     // const wasUpdated = await fb.updateById(id, body)
-    const wasUpdated = await db.updateById(id, body)
-
+    const wasUpdated = await db.updateById(id, body);
 
     wasUpdated
-      ? res.status(200).json({ success: 'product updated' })
-      : res.status(404).json({ error: 'product not found' })
+      ? res.status(200).json({ success: "product updated" })
+      : res.status(404).json({ error: "product not found" });
   } else {
     res.json({
       error: -1,
-      descripcion: 'ruta api/productos metodo post no autorizada',
-    })
+      descripcion: "ruta api/productos metodo post no autorizada",
+    });
   }
-})
+});
 
 // DELETE /api/productos/:id
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   if (admin) {
-    const { id } = req.params
+    const { id } = req.params;
     // const wasDeleted = await contenedor.deleteById(id)
     // const wasDeleted = await fb.deleteById(id)
-    const wasDeleted = await db.deleteById(id)
+    const wasDeleted = await db.deleteById(id);
 
     wasDeleted
-      ? res.status(200).json({ success: 'product successfully removed' })
-      : res.status(404).json({ error: 'product not found' })
+      ? res.status(200).json({ success: "product successfully removed" })
+      : res.status(404).json({ error: "product not found" });
   } else {
     res.json({
       error: -1,
-      descripcion: 'ruta api/productos metodo post no autorizada',
-    })
+      descripcion: "ruta api/productos metodo post no autorizada",
+    });
   }
-})
+});
 
-export const ProductosRoutes = router
+export const ProductosRoutes = router;
